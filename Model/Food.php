c1e1747a91d7c887a8f1c87df3a2ac893361babf
Oleksandra Ubska <alessandraubska@gmail.com>
2024-09-04 17:36:00 +0200

    feat: add food class extends

---
+++ Model/Food.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once __DIR__ . '/Product.php';
+
+// Classe per i cibi
+class Food extends Product
+{
+    public $expirationDate;
+
+    public function __construct($title, $price, $category, $expirationDate)
+    {
+        parent::__construct($title, $price, $category);
+        $this->expirationDate = $expirationDate;
+    }
+}
