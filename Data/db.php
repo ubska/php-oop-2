77db07896c551641b7534560075055fe935b0b9b
Oleksandra Ubska <alessandraubska@gmail.com>
2024-09-04 17:42:54 +0200

    feat: create db

---
+++ Data/db.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once __DIR__ . '/../Model/Food.php';
+require_once __DIR__ . '/../Model/Product.php';
+
+
+$products = [
+
+    new Food("Cibo per Cani", 19.99, "Dog", "2025-12-31"),
+    new Food("Cibo per Gatti", 15.99,  "Cat", "2024-06-30"),
+
+];
