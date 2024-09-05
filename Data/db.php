0655c50c38fbf45838cad3a5757074a1bdcd84b1
Oleksandra Ubska <alessandraubska@gmail.com>
2024-09-05 15:43:49 +0200

    Added checks for title and price of products

--- Data/db.php
+++ Data/db.php
@@ -6,6 +6,8 @@ require_once __DIR__ . '/../Model/Product.php';
 
 $products = [
 
+    // new Food("Cibo per Gatti", -19.99, "Cat", "2026-07-5"),
+    new Food("Cibo per Cani", 30.99, "Dog", "2025-12-31"),
     new Food("Cibo per Cani", 19.99, "Dog", "2025-12-31"),
     new Food("Cibo per Gatti", 15.99,  "Cat", "2024-06-30"),
     new Toy("Pallina per Cani", 5.99, "Dog", "Rubber"),
--- Model/Product.php
+++ Model/Product.php
@@ -14,5 +14,19 @@ class Product
         $this->price = $price;
         $this->category = $category;
         $this->type = $type;
+
+
+
+        // Controllo che il titolo non sia vuoto
+        if (empty($title)) {
+            // Se il titolo è vuoto, lanciamo un errore
+            throw new Exception("Errore: Il titolo non può essere vuoto.");
+        }
+
+        // Controllo che il prezzo sia maggiore di 0
+        if ($price <= 0) {
+            // Se il prezzo è negativo o zero, lanciamo un errore
+            throw new Exception("Errore: Il prezzo deve essere positivo.");
+        }
     }
 }
